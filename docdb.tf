41c94f737adbca9657ad3c84310ccd0bc89e7b89
sharanya chandu <you@example.com>
2023-06-20 10:25:13 +0530

    auto save

--- docdb.tf
+++ docdb.tf
@@ -1,12 +1,12 @@
-resource "aws_docdb_cluster" "docdb" {
+/*resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "robot-${var.ENV}-docdb"
   engine                  = "docdb"
   master_username         = "admin1"
-  master_password         = "roboshop1"
-  skip_final_snapshot     = true
+  master_password         = "roboshop"
+  #skip_final_snapshot     = true
   #db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet_group.name
   #vpc_security_group_ids  = [aws_security_group.allow_mongodb.id]
-}
+}*/
 
 # Creates Subnet Group Needed to host the docdb cluster 
 resource "aws_docdb_subnet_group" "docdb_subnet_group" {
@@ -16,4 +16,7 @@ resource "aws_docdb_subnet_group" "docdb_subnet_group" {
   tags = {
     Name = "robot-${var.ENV}-dodb-subnet-group"
   }
+}
+output "output-ref" {
+    value = data.terraform_remote_state.vpc
 }
