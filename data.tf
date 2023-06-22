75a8adc870028c54fea44c0f1b9997aa085fa83a
sharanya chandu <you@example.com>
2023-06-22 18:09:03 +0530

    auto save

--- data.tf
+++ data.tf
@@ -2,12 +2,12 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "b53-tfstate-bucket"
+    bucket = "b53-s3-bucket"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
    }
 }
-
+/*
 data "aws_secretsmanager_secret" "secrets" {
    name   =  "roboshop/secrets"
 }
@@ -15,6 +15,7 @@ data "aws_secretsmanager_secret" "secrets" {
 data "aws_secretsmanager_secret_version" "secrets" {
   secret_id     = data.aws_secretsmanager_secret.secrets.id
 }
+*/
 
 # output "data" {
 #     value = data.aws_secretsmanager_secret.secrets
