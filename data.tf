5f0f5d67749467c35f69db344b2082f03d01b9b0
sharanya chandu <you@example.com>
2023-06-26 20:35:01 +0530

    auto save

--- data.tf
+++ data.tf
@@ -7,15 +7,15 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
    }
 }
-/*
-data "aws_secretsmanager_secret" "secrets" {
-   name   =  "roboshop/secrets"
-}
 
-data "aws_secretsmanager_secret_version" "secrets" {
-  secret_id     = data.aws_secretsmanager_secret.secrets.id
-}
-*/
+#data "aws_secretsmanager_secret" "secrets" {
+   name   =  "roboshop/secrets"
+#}
+
+#data "aws_secretsmanager_secret_version" "secrets" {
+ # secret_id     = data.aws_secretsmanager_secret.secrets.id
+#}
+
 
 # output "data" {
 #     value = data.aws_secretsmanager_secret.secrets
