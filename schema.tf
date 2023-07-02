512da32fbb2d224be66df1edaeb903804972e258
sharanya chandu <you@example.com>
2023-07-02 23:51:19 +0530

    auto save

--- schema.tf
+++ schema.tf
@@ -3,7 +3,7 @@ resource "null_resource" "schema" {
 
     depends_on = [aws_docdb_cluster.docdb, aws_docdb_cluster_instance.cluster_instances]
 
-  provisioner "local-exec"{
+  provisioner "local-exec" {
         command = <<EOF
         cd /tmp 
         curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
