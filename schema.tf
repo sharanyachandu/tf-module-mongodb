28a9d0902548980f4f87972c6e289628f90929f7
sharanya chandu <you@example.com>
2023-07-02 23:24:16 +0530

    auto save

--- schema.tf
+++ schema.tf
@@ -10,8 +10,8 @@ resource "null_resource" "schema" {
         unzip -o mongodb.zip
         cd mongodb-main 
         wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1  < catalogue.js
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1  < users.js
+        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username centos --password DevOps321  < catalogue.js
+        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username centos --password DevOps321  < users.js
         EOF
     }
 
