d2b020cff82d3f0b58d45e548db4b25a14cff717
sharanya chandu <you@example.com>
2023-07-04 00:04:05 +0530

    auto save

--- docdb.tf
+++ docdb.tf
@@ -1,8 +1,8 @@
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "robot-${var.ENV}-docdb"
-  engine                  = "docdb"
-  master_username         = "centos"
-  master_password         = "DevOps321"
+  engine                  = "docdb"  
+  master_username         = local.DOCDB_USER
+  master_password         = local.DOCDB_PASS
   skip_final_snapshot     =  true
   db_subnet_group_name    =  aws_docdb_subnet_group.docdb_subnet_group.name
   vpc_security_group_ids  =  [aws_security_group.allow_mongodb.id]
